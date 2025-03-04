b81ffcfa69e6f6e183b0125f9fe51cc3f920593b
Logan Born <lcborn4@gmail.com>
2025-03-04 17:15:34 -0500

    under construction component for features

--- src/app/modules/macros/macros.routes.ts
+++ src/app/modules/macros/macros.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
-import { MacrosComponent } from './macros.component';
+import { UnderConstructionComponent } from 'app/layout/common/under-construction/under-construction/under-construction.component';
 
 export default [
+    // {
+    //     path: 'view',
+    //     component: MacrosComponent,
+    // },
+    // {
+    //     path: 'create',
+    //     component: MacrosComponent,
+    // },
+    // {
+    //     path: '',
+    //     component: MacrosComponent,
+    // },
     {
-        path: 'view',
-        component: MacrosComponent,
-    },
-    {
-        path: 'create',
-        component: MacrosComponent,
+        path: '',
+        component: UnderConstructionComponent,
     },
 ] as Routes;
