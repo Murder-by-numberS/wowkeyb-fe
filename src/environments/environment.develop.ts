12383808f203c0f12458120d44a87be3ca97fed2
Logan Born <lcborn4@gmail.com>
2025-10-24 15:56:06 -0400

    oops develop api url

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     envName: 'develop',
-    apiUrl: 'http://localhost:1337/api',
+    apiUrl: 'https://develop-api.wowkeyb.gg/api',
     hmr: false,
     // Force rebuild to ensure correct API URL is used
     debugFlag: 'DEVELOP_ENV_ACTIVE_2024'
